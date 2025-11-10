3a42f2122757d2aada602e864a39f437307634e9
Pablo Barcellos Soares <pablobarcellossoares@gmail.com>
2025-11-10 15:34:32 -0300

    Windows Added

--- Projeto_UVV-Fintech/Views/MainWindow.xaml.cs
+++ Projeto_UVV-Fintech/Views/MainWindow.xaml.cs
@@ -16,9 +16,34 @@ namespace Projeto_UVV_Fintech.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            ViewTransacoes janelaTransacoes = new();
+            janelaTransacoes.ShowDialog();
+            this.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            ViewContas janelaContas = new();
+            janelaContas.ShowDialog();
+            this.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            ViewClientes janelaContas = new();
+            janelaContas.ShowDialog();
+            this.Show();
         }
     }
 }
